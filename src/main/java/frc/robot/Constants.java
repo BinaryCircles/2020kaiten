2d19fc770e52b4a46a80a6c654987411f78603f4
Aryan Jha <aryan@jha.io>
2020-01-12 00:48:07 -0800

    fix formatting in intake, refactor constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,27 +15,21 @@ public final class Constants {
     public static final int DRIVE_BL = 4;
     public static final int DRIVE_BR = 8;
 
+    public static final int CURRENT_LIMIT_AMPS_DRIVE = 40;
+
     // flywheel
     public static final int FLYWHEEL_MAIN = 2;
     public static final int FLYWHEEL_SECONDARY = 3;
 
-    //intake
+    public static final int CURRENT_LIMIT_AMPS_FLYWHEEL = 40;
+    public static final int FLYWHEEL_SPEED = 1;
+
+    // intake
     public static final int INTAKE_MAIN = 6;
 
-    //pneumatic
     public static final int INTAKE_ARM_FRONT = 11;
     public static final int INTAKE_ARM_BACK = 12;
 
-    // drivetrain current limiting
-    public static final int CURRENT_LIMIT_AMPS_DRIVE = 40;
-
-    // flywheel current limiting
-    public static final int CURRENT_LIMIT_AMPS_FLYWHEEL = 40;
-
-    // flywheel speed
-    public static final int FLYWHEEL_SPEED = 1;
-
-    // intake speed
     public static final int INTAKE_SPEED = 1;
 
     // controller IDs
