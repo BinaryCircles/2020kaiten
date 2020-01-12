a3913e19d6eec6049808902cb88cf5b16e743b4d
Porohart <56803529+Porohart@users.noreply.github.com>
2020-01-11 18:51:43 -0800

    climber code including manual control of deploy and winch, PID controller for deploying the climb, a few placeholders that need to be updated

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Climb;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.chassis.Chassis;
@@ -18,10 +20,12 @@ public class RobotContainer {
 
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   public static Chassis s_chassis;
+  public static Climb s_climb;
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   public static final XboxController driveController = new XboxController(1);
+  public static final XboxController operatorController = new XboxController(2);
 
   public RobotContainer() {
     s_chassis = new Chassis();
@@ -30,7 +34,13 @@ public class RobotContainer {
   }
 
   private void configureButtonBindings() {
-
+    new JoystickButton(operatorController, XboxController.Button.kA.value).whenPressed(s_climb::startDeployClimbArm)
+            .whenReleased(s_climb::stopDeployClimbArm);
+    new JoystickButton(operatorController, XboxController.Button.kB.value).whenPressed(s_climb::startWinch)
+            .whenReleased(s_climb::stopWinch);
+    new JoystickButton(operatorController, operatorController.getPOV(0)).whenPressed(() -> s_climb.calculateInput(5));
+    new JoystickButton(operatorController, operatorController.getPOV(90)).whenPressed(() -> s_climb.calculateInput(4));
+    new JoystickButton(operatorController, operatorController.getPOV(180)).whenPressed(() -> s_climb.calculateInput(3));
   }
 
   public static double getTriggerOutput(XboxController controller) {
---
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -0,0 +1,110 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.CounterBase;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.controller.ArmFeedforward;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Climb extends ProfiledPIDSubsystem {
+
+  // initialize the motors
+  private final WPI_TalonSRX deployTalon;
+  private final WPI_TalonSRX winchTalon;
+  private final WPI_VictorSPX deployVictor;
+  private final WPI_VictorSPX winchVictor;
+  private final Encoder climbArmEncoder;
+  private double lastError;
+  private double error;
+  private double diffError;
+
+  // constructor
+  public Climb()  {
+    super( new ProfiledPIDController(Constants.ARM_kP, Constants.ARM_kI, Constants.ARM_kD,
+            new TrapezoidProfile.Constraints(Constants.MAX_VELOCITY_RAD_PER_SEC, Constants.MAX_ACCEL)), 0);
+
+    // define the motor controllers
+    deployTalon = new WPI_TalonSRX(Constants.DEPLOY_TALON);
+    deployVictor = new WPI_VictorSPX(Constants.DEPLOY_VICTOR);
+    winchTalon = new WPI_TalonSRX(Constants.WINCH_TALON);
+    winchVictor = new WPI_VictorSPX(Constants.WINCH_VICTOR);
+
+    // set victors to follow talons
+    deployVictor.follow(deployTalon);
+    winchVictor.follow(winchTalon);
+
+    // initialize encoder
+    climbArmEncoder = new Encoder(Constants.CLIMB_DEPLOY_DIO, Constants.CLIMB_DEPLOY_DIO2);
+
+
+    // set dpp of encoder
+    climbArmEncoder.setDistancePerPulse(Constants.DISTANCE_PER_PULSE);
+
+    lastError = 0;
+    error = 0;
+    diffError = 0;
+  }
+
+  public void climbArmDeployPID(double input) {
+    while(Math.abs(error) > 5) {
+      error = input - climbArmEncoder.get();
+      diffError = error - lastError;
+      double output = Constants.ARM_kP * error + Constants.ARM_kD * diffError + Constants.ARM_kF_GRAV;
+      deployTalon.set(output);
+      lastError = error;
+    }
+  }
+
+  public void calculateInput(double input) {
+    double output = (Math.asin(input/(Constants.CLIMB_ARM_LENGTH_1 + Constants.CLIMB_ARM_LENGTH_2)));
+    climbArmDeployPID(output);
+  }
+
+  @Override
+  public void useOutput(double output, TrapezoidProfile.State setpoint) {
+
+  }
+
+  @Override
+  public double getMeasurement() {
+    return climbArmEncoder.getDistance() + Constants.ARM_OFFSET;
+  }
+
+
+
+  public void startDeployClimbArm() {
+    deployTalon.set(Constants.DEPLOY_SPEED);
+  }
+
+  public void stopDeployClimbArm() {
+    deployTalon.set(0);
+  }
+
+  public void startWinch() {
+    winchTalon.set(Constants.WINCH_SPEED);
+  }
+
+  public void stopWinch() {
+    winchTalon.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
