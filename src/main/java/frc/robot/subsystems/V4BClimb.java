3bda9e91bbc8b2bb79d757c61b374a17d3478d53
Porohart <56803529+Porohart@users.noreply.github.com>
2020-01-14 11:09:24 -0800

    Added a new subsystem in case of elevator climb, added a subclass in Constants for climb constants

--- src/main/java/frc/robot/subsystems/V4BClimb.java
+++ src/main/java/frc/robot/subsystems/V4BClimb.java
@@ -14,9 +14,9 @@ import edu.wpi.first.wpilibj.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
-import frc.robot.Constants;
+import frc.robot.Constants.ClimbConstants;
 
-public class Climb extends ProfiledPIDSubsystem {
+public class V4BClimb extends ProfiledPIDSubsystem {
 
   // initialize the motors
   private final WPI_TalonSRX deployTalon;
@@ -31,29 +31,29 @@ public class Climb extends ProfiledPIDSubsystem {
   private final ArmFeedforward climbArmFeedForward;
 
   // constructor
-  public Climb()  {
-    super(new ProfiledPIDController(Constants.ARM_kP, Constants.ARM_kI, Constants.ARM_kD,
-            new TrapezoidProfile.Constraints(Constants.MAX_VELOCITY_RAD_PER_SEC, Constants.MAX_ACCEL)), 0);
-    controller = new ProfiledPIDController(Constants.ARM_kP, Constants.ARM_kI, Constants.ARM_kD,
-            new TrapezoidProfile.Constraints(Constants.MAX_VELOCITY_RAD_PER_SEC, Constants.MAX_ACCEL));
+  public V4BClimb()  {
+    super(new ProfiledPIDController(ClimbConstants.ARM_kP, ClimbConstants.ARM_kI, ClimbConstants.ARM_kD,
+            new TrapezoidProfile.Constraints(ClimbConstants.MAX_VELOCITY_RAD_PER_SEC, ClimbConstants.MAX_ACCEL)), 0);
+    controller = new ProfiledPIDController(ClimbConstants.ARM_kP, ClimbConstants.ARM_kI, ClimbConstants.ARM_kD,
+            new TrapezoidProfile.Constraints(ClimbConstants.MAX_VELOCITY_RAD_PER_SEC, ClimbConstants.MAX_ACCEL));
     // define the motor controllers
-    deployTalon = new WPI_TalonSRX(Constants.DEPLOY_TALON);
-    deployVictor = new WPI_VictorSPX(Constants.DEPLOY_VICTOR);
-    winchTalon = new WPI_TalonSRX(Constants.WINCH_TALON);
-    winchVictor = new WPI_VictorSPX(Constants.WINCH_VICTOR);
+    deployTalon = new WPI_TalonSRX(ClimbConstants.DEPLOY_TALON);
+    deployVictor = new WPI_VictorSPX(ClimbConstants.DEPLOY_VICTOR);
+    winchTalon = new WPI_TalonSRX(ClimbConstants.WINCH_TALON);
+    winchVictor = new WPI_VictorSPX(ClimbConstants.WINCH_VICTOR);
 
     // set victors to follow talons
     deployVictor.follow(deployTalon);
     winchVictor.follow(winchTalon);
 
     // initialize encoder
-    climbArmEncoder = new Encoder(Constants.CLIMB_DEPLOY_DIO, Constants.CLIMB_DEPLOY_DIO2);
+    climbArmEncoder = new Encoder(ClimbConstants.CLIMB_DEPLOY_DIO, ClimbConstants.CLIMB_DEPLOY_DIO2);
 
     // initialize feedforward
-    climbArmFeedForward = new ArmFeedforward(Constants.kS, Constants.kV, Constants.kA);
+    climbArmFeedForward = new ArmFeedforward(ClimbConstants.kS, ClimbConstants.kV, ClimbConstants.kA);
 
     // set dpp of encoder
-    climbArmEncoder.setDistancePerPulse(Constants.DISTANCE_PER_PULSE);
+    climbArmEncoder.setDistancePerPulse(ClimbConstants.DISTANCE_PER_PULSE);
 
     lastError = 0;
     error = 0;
@@ -72,7 +72,7 @@ public class Climb extends ProfiledPIDSubsystem {
   } */
 
   public void calculateInput(double input) {
-    double output = (Math.asin(input/(Constants.CLIMB_ARM_LENGTH_1 + Constants.CLIMB_ARM_LENGTH_2)));
+    double output = (Math.asin(input/(ClimbConstants.CLIMB_ARM_LENGTH_1 + ClimbConstants.CLIMB_ARM_LENGTH_2)));
     goToSetpoint(output);
   }
 
@@ -91,13 +91,13 @@ public class Climb extends ProfiledPIDSubsystem {
 
   @Override
   public double getMeasurement() {
-    return climbArmEncoder.getDistance() + Constants.ARM_OFFSET;
+    return climbArmEncoder.getDistance() + ClimbConstants.ARM_OFFSET;
   }
 
 
 
   public void startDeployClimbArm() {
-    deployTalon.set(Constants.DEPLOY_SPEED);
+    deployTalon.set(ClimbConstants.DEPLOY_SPEED);
 }
 
   public void stopDeployClimbArm() {
@@ -105,7 +105,7 @@ public class Climb extends ProfiledPIDSubsystem {
   }
 
   public void startWinch() {
-    winchTalon.set(Constants.WINCH_SPEED);
+    winchTalon.set(ClimbConstants.WINCH_SPEED);
   }
 
   public void stopWinch() {
