c25e052aae63463375b9a37529688eb4c8ffd010
Jason <jason.a.miller@live.com>
2020-01-13 22:50:45 -0800

    rebased cameraOutput off of master and reformatted constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -119,8 +119,14 @@ public final class Constants {
     public static final int camera1_dev_num = 0;
     public static final int camera2_dev_num = 1;
 
-    public static final int camera1_resolution_x = 640;
-    public static final int camera1_resolution_y = 480;
-    public static final int camera2_resolution_x = 640;
-    public static final int camera2_resolution_y = 480;
+    public final class camera{
+        public static final int camera1_dev_num = 0;
+        public static final int camera2_dev_num = 1;
+
+        public static final int camera1_resolution_x = 640;
+        public static final int camera1_resolution_y = 480;
+        public static final int camera2_resolution_x = 640;
+        public static final int camera2_resolution_y = 480;
+}
+
 }
