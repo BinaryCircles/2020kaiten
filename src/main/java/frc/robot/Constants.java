07b3f44873177263ff1b2afeb4099284dc17c208
Everett Wang <everettwang@outlook.com>
2020-01-17 18:35:48 -0800

    created ramsete command, and an example trajectory

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,14 +7,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+
 public final class Constants {
 
-    public final class Drivetrain {
+    public static final class Drivetrain {
         // drivetrain
         public static final int drive_fl = 5;
         public static final int drive_fr = 1;
         public static final int drive_bl = 4;
         public static final int drive_br = 8;
+
+        // kS = volts; kV = seconds / meter; kA = seconds^2 / meter
+        public static final double kS = 0;
+        public static final double kV = 0;
+        public static final double kA = 0;
+
+        public static final double trackWidth = 0;
+        public static final DifferentialDriveKinematics driveKinematics = new DifferentialDriveKinematics(trackWidth);
+
+        // ramsete constants, ramseteB and Zeta are in meters per second
+        public static final int maxVoltage = 10;
+        public static final double ramseteB = 2;
+        public static final double ramseteZeta = 0.7;
+
+        // max speed / accel
+        public static final double maxSpeedMetersPerSecond = 69;
+        public static final double maxAccelMetersPerSecondSquared = 420;
+
+        // kP velocity
+        public static final double kPVel = 10;
     }
 
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,24 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.chassis.Chassis;
+
+import java.util.List;
 
 public class RobotContainer {
 
@@ -46,6 +60,49 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return m_autoCommand;
+
+    // sets voltage constraint so you dont over accelerate
+    var voltageConstraint = new DifferentialDriveVoltageConstraint(new SimpleMotorFeedforward(Constants.Drivetrain.kS,
+            Constants.Drivetrain.kV, Constants.Drivetrain.kA), Constants.Drivetrain.driveKinematics,
+            Constants.Drivetrain.maxVoltage);
+
+    // Create config for trajectory
+    TrajectoryConfig config =
+            new TrajectoryConfig(Constants.Drivetrain.maxSpeedMetersPerSecond,
+                    Constants.Drivetrain.maxAccelMetersPerSecondSquared)
+                    // Add kinematics to ensure max speed is actually obeyed
+                    .setKinematics(Constants.Drivetrain.driveKinematics)
+                    // Apply the voltage constraint
+                    .addConstraint(voltageConstraint);
+
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            // Pass through these two interior waypoints, making an 's' curve path
+            List.of(
+                    new Translation2d(1, 1),
+                    new Translation2d(2, -1)
+            ),
+            // End 3 meters straight ahead of where we started, facing forward
+            new Pose2d(3, 0, new Rotation2d(0)),
+            // Pass config
+            config
+    );
+
+    RamseteCommand ramseteCommand = new RamseteCommand(
+            exampleTrajectory,
+            s_chassis::getPose,
+            new RamseteController(Constants.Drivetrain.ramseteB, Constants.Drivetrain.ramseteZeta),
+            new SimpleMotorFeedforward(Constants.Drivetrain.kS, Constants.Drivetrain.kV, Constants.Drivetrain.kA),
+            Constants.Drivetrain.driveKinematics,
+            s_chassis::getWheelSpeeds,
+            new PIDController(Constants.Drivetrain.kPVel, 0, 0),
+            new PIDController(Constants.Drivetrain.kPVel, 0, 0)
+            // return the volts
+            s_chassis::tankDriveVolts,
+            s_chassis
+    );
+
+    return ramseteCommand.andThen(() -> s_chassis.tankDriveVolts(0,0));
   }
 }
